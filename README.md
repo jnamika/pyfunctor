62f3e386e170bffffe20c603bfdc2714945099c1
Jun Namikawa <jnamika@gmail.com>
2013-09-23 19:53:07 +0900

    modified an example in README.md and src/__init__.py

--- README.md
+++ README.md
@@ -10,11 +10,14 @@ pyfunctor is a Python Functor library that provides classes implementing lazy ev
 An example of usages is following:
 
     >>> from pyfunctor.functor import *
-    >>> f = Functor(range(10)) >> c_(map)(lambda x: x * 2)
-    ...                        >> c_(filter)(lambda x: x < 7)
-    ...                        >> c_(sorted).key(lambda x: -x)  # the pipeline operator '>>' composes functions
+    >>> f = (Functor(range(10)) >> c_(map)(lambda x: x * 2)
+    ...      >> c_(filter)(lambda x: x < 7)
+    ...      >> c_(sorted).key(lambda x: -x))
     >>> run(f)  # lazy evaluation
     [6, 4, 2, 0]
+
+The 'Functor' class packs a value into a context.
+The pipeline operator '>>' composes functions, but it is not calculated until 'run' function is applied to the Functor object.
 
 Furthermore, Functor object will work together with the 'with' statement.
 The object is only once evaluated after the with-block is done.
@@ -25,6 +28,7 @@ The object is only once evaluated after the with-block is done.
     ...         y = x % 3
     ...         z = x + y
     ...         return x + y + z
+    ...
     ...     @c_(sorted, keyword='key')
     ...     def g(x):
     ...         return (x % 7, x % 3, x)
--- src/__init__.py
+++ src/__init__.py
@@ -6,11 +6,15 @@ lazy evaluation, pipeline operators and block syntax.
 An example of usages is following:
 
     >>> from functor import *
-    >>> f = Functor(range(10)) >> c_(map)(lambda x: x * 2)
-    ...                        >> c_(filter)(lambda x: x < 7)
-    ...                        >> c_(sorted).key(lambda x: -x)  # the pipeline operator '>>' composes functions
+    >>> f = (Functor(range(10)) >> c_(map)(lambda x: x * 2)
+    ...      >> c_(filter)(lambda x: x < 7)
+    ...      >> c_(sorted).key(lambda x: -x))
     >>> run(f)  # lazy evaluation
     [6, 4, 2, 0]
+
+The 'Functor' class packs a value into a context.
+The pipeline operator '>>' composes functions, but it is not calculated until
+'run' function is applied to the Functor object.
 
 Furthermore, Functor object will work together with the 'with' statement.
 The object is only once evaluated after the with-block is done.
@@ -21,6 +25,7 @@ The object is only once evaluated after the with-block is done.
     ...         y = x % 3
     ...         z = x + y
     ...         return x + y + z
+    ...
     ...     @c_(sorted, keyword='key')
     ...     def g(x):
     ...         return (x % 7, x % 3, x)
@@ -30,5 +35,5 @@ The object is only once evaluated after the with-block is done.
 
 __author__ = 'Jun Namikawa'
 __email__ = 'jnamika@gmail.com'
-__version__ = '0.1'
+__version__ = '0.1.1'
 __license__ = 'ISC License (ISCL)'
